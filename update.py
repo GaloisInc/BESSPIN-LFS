02b3a3a27a9014851adaf71a09bba6766746b665
Isaiah Mindich <isaiah@galois.com>
2020-06-01 13:35:54 -0700

    Add script to copy binaries from Nix store

---
+++ update.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# This script will not work unless it is run with the FETT Nix shell.
+
+import os, shutil
+from itertools import product
+
+systems = ["debian", "FreeBSD", "busybox"]
+testingPlatforms = ["fpga", "qemu"]
+processors = ["chisel_p1", "chisel_p2", "bluespec_p2"]
+extensions = ["bit", "ltx"]
+
+def bitfilePaths():
+    bitfileVar = 'FETT_GFE_BITFILE_DIR'
+    if bitfileVar not in os.environ:
+        print("Error: bitfile directory not in Nix environment.")
+        exit(1)
+    bitfileDir = os.environ[bitfileVar]
+    bitfiles = [ f"soc_{proc}.{ext}" for proc, ext in product(processors, extensions)]
+    return [(os.path.join(bitfileDir, path), os.path.join('bitfiles', 'fpga', path))
+            for path in bitfiles]
+
+def imagePaths():
+    pairs = []
+    for system, platform in product(systems, testingPlatforms):
+        imageVar = f"FETT_GFE_{system.upper()}_{platform.upper()}"
+        if imageVar not in os.environ:
+            print(f"Error: OS image for {system} on {platform} not in Nix environment.")
+            exit(1)
+        pairs.append((os.environ[imageVar], os.path.join('osImages', platform, system + '.elf')))
+    return pairs
+
+def copyFromNix(baseDir):
+    if baseDir is None:
+        prefix = 'GFE'
+    else:
+        prefix = os.path.join(baseDir, 'GFE')
+    paths = bitfilePaths() + imagePaths()
+    print("Copying files from Nix store")
+    for src, dest in paths:
+        destFullPath = os.path.join(prefix, dest)
+        try:
+            shutil.copy(src, destFullPath)
+            os.chmod(destFullPath, 0o664) # Files in the nix store are read only
+            print(src, "->", os.path.join(prefix, destFullPath))
+        except Exception as e:
+            print(f"Error when copying {src} to {dest}")
+            exit(1)
+
+copyFromNix(None)
